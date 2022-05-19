f0f106920f2bd6b8af89034b0a54a871a9b29f4d
D-Hello2 <102397298+D-Hello2@users.noreply.github.com>
2022-05-19 20:37:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,23 +33,23 @@ read -p "number of interfaces: " numint
 
 for i in {1..$numint};
 do
-  read -p "name interfaces: " int1
-  read -p "ip address ( example: 10.10.10.1/24 ): " ip1
-  read -p "gateway: " gateway1
-  read -p "dns: " dns1
-  sudo cat << EOF | sudo tee /etc/netplan/00-installer-config.yaml
-  network:
-    ethernets:
-      $int1:
-        dhcp4: no
-        addresses:
-          - $ip1
-        gateway4: $gateway1
-        nameservers:
-          addresses: [$dns1] 
-  EOF
-  echo ""
-  sleep 1
+	read -p "name interfaces: " int1
+	read -p "ip address ( example: 10.10.10.1/24 ): " ip1
+	read -p "gateway: " gateway1
+	read -p "dns: " dns1
+	sudo cat << EOF | sudo tee /etc/netplan/00-installer-config.yaml
+	network:
+	     ethernets:
+	      $int1:
+	        dhcp4: no
+	         addresses:
+	          - $ip1
+	        gateway4: $gateway1
+	        nameservers:
+	          addresses: [$dns1] 
+	EOF
+	echo ""
+	sleep 1
 done
 
 echo "restart network"
@@ -60,24 +60,24 @@ sleep 1
 echo "mapping host"
 for ii in {1..$numint};
 do
-  sudo cat << EOF | sudo tee -a /etc/hosts
-  $(hostname -I | awk '{print $ii}') $HOSTNAME 
-  EOF
-  echo ""
-  sleep 1
+	sudo cat << EOF | sudo tee -a /etc/hosts
+	$(hostname -I | awk '{print $ii}') $HOSTNAME 
+	EOF
+	echo ""
+	sleep 1
 done
 
 read -p "do you want to update the system and install some packages? (y/n) : " update1
 if [ $update1 == "y" && $update1 == "Y" ] 
-  read -p "name packages ( example: apache2 w3m ): " packages1
-  echo "update"
-  sudo apt update
-  sudo apt install $packages1 -y
+	read -p "name packages ( example: apache2 w3m ): " packages1
+	echo "update"
+	sudo apt update
+	sudo apt install $packages1 -y
 
 elif [ $update1 == "n" && $update1 == "N" ]
-  echo "ok"
+	echo "ok"
 
 else
-  echo "please input y/n"
+	echo "please input y/n"
 
 done
